16d51c0d9f3a346d953c8e7c3492e5ec2413e2a1
Kushang Gajjar <g.kushang@gmail.com>
2017-06-19 15:32:58 -0400

    support for cucumber2

--- test/features/step_definitions/test.js
+++ test/features/step_definitions/test.js
@@ -1,74 +1,73 @@
-var steps = function() {
-    this.Before(function(scenario, callback) {
+var {defineSupportCode} = require('cucumber');
+
+defineSupportCode(function ({Before, Given, Then}) {
+
+    Before(function(scenario, callback) {
         console.log('console logs should not break the report');
-        this.scenario = scenario;
         callback();
     });
 
-    this.Then(/^this feature runs with background$/, function(callback) {
+    Then(/^this feature runs with background$/, function(callback) {
         callback();
     });
 
-    this.Then(/^Fred runs a passing cucumber scenario$/, function(callback) {
+    Then(/^Fred runs a passing cucumber scenario$/, function(callback) {
         callback();
     });
 
-    this.Given(/^Fred runs a passing cucumber scenario on behalf of "([^"]*)"/, function(name, callback) {
+    Given(/^Fred runs a passing cucumber scenario on behalf of "([^"]*)"/, function(name, callback) {
         callback();
     });
 
-    this.Then(/^he choose "([^"]*)" output as one of the formatter$/, function(arg1, callback) {
+    Then(/^he choose "([^"]*)" output as one of the formatter$/, function(arg1, callback) {
         callback();
     });
 
-    this.Then(/^the output should contain test results in HTML format$/, function(callback) {
+    Then(/^the output should contain test results in HTML format$/, function(callback) {
         callback();
     });
 
-    this.Then(/^Fred runs a failing cucumber scenario$/, function(callback) {
+    Then(/^Fred runs a failing cucumber scenario$/, function(callback) {
         callback();
     });
 
-    this.Then(/^a failing scenario captures a screenshot$/, function(callback) {
-        this.scenario.attach(new Buffer('').toString('base64'), 'image/png');
+    Then(/^a failing scenario captures a screenshot$/, function(callback) {
+        this.attach('', 'image/png');
         callback();
     });
 
-    this.Then(/^the output should contain test results with screenshot in HTML format$/, function(callback) {
+    Then(/^the output should contain test results with screenshot in HTML format$/, function(callback) {
         callback();
     });
 
-    this.Then(/^he left one of the step as a pending$/, function(callback) {
+    Then(/^he left one of the step as a pending$/, function(callback) {
         callback();
     });
 
-    this.Then(/^the output should contain the pending test in the HTML pie chart$/, function(callback) {
+    Then(/^the output should contain the pending test in the HTML pie chart$/, function(callback) {
         callback(null, 'pending');
     });
 
-    this.Then(/^the output should contain the skipped steps in the HTML pie chart$/, function(callback) {
+    Then(/^the output should contain the skipped steps in the HTML pie chart$/, function(callback) {
         callback();
     });
 
-    this.Then(/^Fred attaches the "([^"]*)" to the Given step of passing cucumber scenario$/, function(testData, callback) {
-        this.scenario.attach(testData);
+    Then(/^Fred attaches the "([^"]*)" to the Given step of passing cucumber scenario$/, function(testData, callback) {
+        this.attach(testData);
         callback();
     });
 
-    this.Then(/^the output should contain test data attached to the Given step in HTML format$/, function(callback) {
+    Then(/^the output should contain test data attached to the Given step in HTML format$/, function(callback) {
         callback();
     });
 
-    this.Given(/^Fred runs a passing scenario for the following data set$/, function(table, callback) {
+    Given(/^Fred runs a passing scenario for the following data set$/, function(table, callback) {
         // Write code here that turns the phrase above into concrete actions
         callback();
     });
 
-    this.Then(/^the output should contain data table attached in HTML format$/, function(callback) {
+    Then(/^the output should contain data table attached in HTML format$/, function(callback) {
         // Write code here that turns the phrase above into concrete actions
         callback();
     });
-};
-
-
-module.exports = steps;
+});
